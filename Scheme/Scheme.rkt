cc664dfbeade8c3ef2813e1188652430b469aca6
Casper Thule <c.thulehansen@live.com>
2014-05-13 13:55:17 +0200

    Added scheme file with the procedures: GenerateSamplePoints and CreateGraphValues

---
+++ Scheme/Scheme.rkt
@@ -0,0 +1,37 @@
+;The procedure below creates sample points based on start x value, stop x value and number of sample points
+(define GenerateSamplePoints
+  (letrec ((GeneratePoints
+            (lambda (Interval Acc Stop)
+            (if (> (+ (car Acc) Interval) Stop)
+                Acc
+                (GeneratePoints Interval (cons (+ (car Acc) Interval) Acc) Stop )))))
+  (lambda (start stop dp)
+    (let ((interval (/ (- stop start) dp)))
+    (if (<= stop start)
+         '()
+         (reverse (GeneratePoints interval (list (+ start (- interval (/ interval 2)))) stop)))))))
+
+
+(define zip
+  (letrec
+      ((innerzip
+        (lambda (lst1 lst2 acc)
+          (if (or (null? lst1) (null? lst2))
+              acc
+              (innerzip (cdr lst1) (cdr lst2) (cons (cons (car lst1) (car lst2)) acc))
+          ))))
+  (lambda (lst1 lst2)
+    (reverse (innerzip lst1 lst2 '())))))
+
+;The procedure creates a list of pairs with x and y values
+;sp is short for sample points, which means the x values
+(define CreateGraphValues
+  (letrec ((createPoints
+            (lambda (func acc sp)
+              (if (null? sp)
+                  acc
+                  (createPoints func (cons (func (car sp)) acc) (cdr sp))))))
+  (lambda (func sp)
+    (zip sp (reverse (createPoints func '() sp))))))
+    
+         

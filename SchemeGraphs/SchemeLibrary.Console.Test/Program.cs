cb199cc30b681194c823de4d2f1e476e56273565
bakacaptain <klogesam@gmail.com>
2014-05-19 17:42:08 +0200

    Added SchemeLibrary.Console.Test for manual test of the library.

---
+++ SchemeGraphs/SchemeLibrary.Console.Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SchemeLibrary.Console.Test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Loading scheme file");
+            var loader = new SchemeLoader();
+            loader.Import("simple.ss");
+            System.Console.WriteLine("Finished loading scheme file");
+
+
+            System.Console.WriteLine("Evaluating scheme statement");
+            var evaluator = new SchemeEvalProxy();
+            var result = evaluator.Evaluate<Int32>("(increment 5)");
+
+            System.Console.WriteLine("The result is: {0}", result);
+
+            System.Console.ReadLine();
+        }
+    }
+}

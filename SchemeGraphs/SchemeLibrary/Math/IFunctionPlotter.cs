d6b42ea110d621ec97db1ff3c392b709aa2c6717
bakacaptain <klogesam@gmail.com>
2014-05-20 14:00:41 +0200

    Added high-level interface for scheme procedures.

---
+++ SchemeGraphs/SchemeLibrary/Math/IFunctionPlotter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using IronScheme.Runtime;
+
+namespace SchemeLibrary.Math
+{
+    interface IFunctionPlotter
+    {
+        Cons PlotFunction(string function, Int32 noOfSamples);
+        Cons PlotDerivative(string function, Int32 noOfSamples);
+        Cons PlotIntegral(string function, Int32 noOfSamples);
+    }
+}
